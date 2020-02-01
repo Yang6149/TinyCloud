ba83fd90b07859e985a093213edd0de88a62ffa3
Yang shen <614989768@qq.com>
2020-02-02 02:46:37 +0800

    init

---
+++ src/main/java/com/example/mycloud/Model/File.java
@@ -0,0 +1,13 @@
+package com.example.mycloud.Model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table
+@Entity
+public class File {
+    
+}
